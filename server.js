b3140f5374ec41d2700007c5b5bc99fdca0168d2
DM2097 <dhanesh2013.500@gmail.com>
2016-10-27 21:52:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var config={
     user: 'ddm2097',
     database: 'ddm2097',
     host: 'db.imad.hasura-app.io',
-    port: '5433',
+    port: '5432',
     password: process.env.DB_PASSWORD
     
 };
