a98aaa77c06a1d963a82dc22449901193b61e1bf
DM2097 <dhanesh2013.500@gmail.com>
2016-11-05 12:08:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/ui/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 app.get('/ui/Portfolio.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Portfolio.html'));
 });
