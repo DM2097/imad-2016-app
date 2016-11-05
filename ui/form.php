b16b781769d0eb1628cae754a8b1f6b1e0244ef0
DM2097 <dhanesh2013.500@gmail.com>
2016-11-05 12:50:34 +0530

    Update form.php

--- ui/form.php
+++ ui/form.php
@@ -2,8 +2,8 @@
 $name=$_POST['name'];
 $email=$_POST['email'];
 $message=$_POST['message'];
-$to='dhanesh2013.500@gmail.com';
-$subject='Website Contact Form';
+$to = 'dhanesh2013.500@gmail.com';
+$subject = 'Website Contact Form';
 $msg= $name . " " .$email . " " . $message;
 /*$headers = "From:" . $email . "\r\n";*/
 mail($to,$subject,$msg);
