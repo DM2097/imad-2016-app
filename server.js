fac7a5bda430ea6834a73ca1585dcc6a0890b66d
DM2097 <dhanesh2013.500@gmail.com>
2016-11-05 11:07:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,106 +1,26 @@
 var express = require('express');
 var morgan = require('morgan');
-var path = require('path');
-var Pool= require('pg').Pool;
-var config={
-    user: 'dm2097',
-    database: 'dm2097',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
-    
-};
-
-
 var app = express();
 app.use(morgan('combined'));
-var articleOne={
-	title:'Article One',
-	heading:'Article One',
-	date:'Sep 5,2016',
-	content:"<p>hhahhahah hahahhah ahhahhaha</p>"
-};
-function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var content=data.content;
-var htmlTemplate=`<html><head><title>${title}</title>
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
-<link href="/ui/style.css" rel="stylesheet" />
 
-</head>
-<body>
-<div class="container">
-<div>
-<div>
-<a href="/">Home</a>
-</div>
-<hr/>
-<div>
-${date}
-</div>
-<div>
-<p>
-${content}
-</p>
-</div>
-</div>
-</body>
-</html>"`;
-return htmlTemplate;
-}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool=new Pool(config);
 
-app.get('/test-db', function(req,res)
-{
-    pool.query('SELECT * FROM test',function(err,result)
-    {
-       if(err)
-       {
-           res.status(500).send(err.toString());
-       }
-       else
-       {
-           res.send(JSON.stringify(result.rows));
-       }
-       
-    });
-});
-var counter=0;
-app.get('/counter', function(req,res){
-    counter=counter+1;
-    res.send(counter.toString());
-});
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleOne));
-});
-app.get('/article-two', function (req, res) {
-  res.send(path.join('Article two requested here'));
-});
-app.get('/article-three', function (req, res) {
-  res.send(path.join('Article three requested here'));
-});
+
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-var names=[];
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-app.get('/submit-name',function(req,res){
-    var name=req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-    
-});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log('IMAD course app listening on port ${port}!')
+  console.log('IMAD course app listening on port', ${port}!)
 });
