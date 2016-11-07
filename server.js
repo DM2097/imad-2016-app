764b01e43e481de90d20797580530a518f082320
DM2097 <dhanesh2013.500@gmail.com>
2016-11-07 14:12:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var crypto=require('crypto');
+var config={
+    user: 'dm2097',
+    database: 'dm2097',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    };
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+function hash(input, salt){
+    var hashed=crypto.pbkdf25ync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+    
+}
+app.get('/hash/:input',function (req, res){
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+    
+});
+
 app.get('/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
